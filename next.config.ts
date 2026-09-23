63ade9cdc30332f6d1078c836c9f8bda07df9fbb
deebuluku-create <deebuluku@gmail.com>
2026-09-23 06:16:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+
+  basePath: "/bingo-app",
+  assetPrefix: "/bingo-app/",
+
   images: {
     unoptimized: true,
   },
